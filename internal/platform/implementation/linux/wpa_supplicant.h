47f1e3d06a0c50f025586b992c53271edae5ab25
kidfromjupiter <limahaliyana@gmail.com>
2026-02-01 06:54:10 +0000

    wpa supplicant stub

---
+++ internal/platform/implementation/linux/wpa_supplicant.h
@@ -0,0 +1,13 @@
+#ifndef WORKSPACE_WPA_SUPPLICANT_H
+#define WORKSPACE_WPA_SUPPLICANT_H
+
+namespace nearby {
+namespace linux {
+namespace wpa_supplicant {
+class WpaSupplicant {
+
+};
+}
+}
+}
+#endif // WORKSPACE_WPA_SUPPLICANT_H
